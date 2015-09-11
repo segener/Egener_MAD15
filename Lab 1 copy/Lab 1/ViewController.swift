217e01b47f861a685af3544a0e0888537a1d41d0
segener <segener@gmail.com>
2015-09-11 16:32:46 -0600

    Lab 1

---
+++ Lab 1 copy/Lab 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab 1
+//
+//  Created by Sarah Egener on 9/11/15.
+//  Copyright (c) 2015 TAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
